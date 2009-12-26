11a24820f3733b96bc759126427bb203240c5041
Mathias Brökelmann <mbroekelmann@googlemail.com>
2009-12-26 22:40:19 +0000

    fix mime.types file lookup add url handlers
    git-svn-id: https://svn.osxp.de/svn/repo/dali/trunk@933 2bc8f7d6-03ee-481e-9765-c164d3918215

--- frontend/resources/src/main/scala/de/osxp/dali/frontend/resources/internal/ResourcesOsgiModule.scala
+++ frontend/resources/src/main/scala/de/osxp/dali/frontend/resources/internal/ResourcesOsgiModule.scala
@@ -20,15 +20,22 @@ import javax.ws.rs.ext.MessageBodyWriter
 class ResourcesOsgiModule extends AbstractModule {
     
     def configure {
-        bind(classOf[FileTypeMap]).toInstance(new MimetypesFileTypeMap)
-
         //bind(classOf[ContentTypes]).to(classOf[FileTypeMapContentTypes]).in(Scopes.SINGLETON)
 
         val provider = service(classOf[FileTypeMapContentTypes]).export
-        bind(classOf[ContentTypes]).toProvider(provider.direct)
+        bind(export(classOf[ContentTypes])).toProvider(provider)
+        bind(classOf[ContentTypes]).toProvider(service(classOf[ContentTypes]).single())
         bind(classOf[BundleExtender]).to(classOf[StaticResourcesBundleExtender]).in(Scopes.SINGLETON)
         
         bind(export(classOf[MessageBodyWriter[Resource]]))
             .toProvider(service(classOf[ResourceMessageWriter]).export)
+    }
+    
+    @Provides
+    def provideFileTypeMap(context: BundleContext): FileTypeMap = {
+        // TODO: collect mime.types files from all deployed bundles
+        val resource = Option(context.getBundle.getResource("META-INF/mime.types"))
+        resource.map(url => new MimetypesFileTypeMap(url.openStream))
+                .getOrElse(new MimetypesFileTypeMap)
     }
 }
