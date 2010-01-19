d01098cc800ba3d85c22851ef36ef2d20c8c7a1e
Mathias Brökelmann <mbroekelmann@googlemail.com>
2010-01-19 09:33:19 +0100

    minor improvments, reordering code

--- bundles/web-links/src/main/java/org/mediahub/web/links/Links.scala
+++ bundles/web-links/src/main/java/org/mediahub/web/links/Links.scala
@@ -15,6 +15,17 @@ import scala.reflect.ClassManifest._
 
 import java.lang.reflect.{ParameterizedType}
 
+class Links(val context: LinkContext) {
+
+  def linkTo[A<:AnyRef](implicit clazz: ClassManifest[A]): ResourceActionLinkBuilder[A] =
+    new ResourceLinkBuilderImpl(context).resolvedBy[A]
+
+
+  def linkTo[A<:AnyRef](target: A)(implicit clazz: ClassManifest[A]): ResourceActionLinkBuilder[A] = {
+    new ResourceLinkBuilderImpl(context).resolvedBy(target)
+  }
+}
+
 object Links extends Links(null) {
   implicit def linkBuilderToString(builder: LinkBuilder): String = {
     builder.build.map(x =>
@@ -39,23 +50,15 @@ object Links extends Links(null) {
   }
 }
 
-object RootResource {
-
-  def unapply[A<:AnyRef](clazz: ClassManifest[A]): Option[Class[A]] = {
-    unapply(clazz.erasure.asInstanceOf[Class[A]])
-  }
-
-  def unapply[A<:AnyRef](clazz: Class[A]): Option[Class[A]] = {
-    Option(clazz.getAnnotation(classOf[Path])).map(x => clazz)
-  }
-}
-
+/**
+ * A link context provides contexutal information for building links.
+ */
 trait LinkContext {
 
   /**
-   * resolve the UriBuilder initialized with the base uri for the given class type.
+   * resolve the base uri for the given class type.
    */
-  def baseUri[A<:AnyRef](implicit clazz: ClassManifest[A]): Option[UriBuilder] = None
+  def baseUri[A<:AnyRef](implicit clazz: ClassManifest[A]): Option[URI] = None
 
   def resolver: Seq[LinkResolver[_]] = Seq.empty
 
@@ -72,25 +75,125 @@ trait LinkContext {
    */
   def baseUriBuilder[A<:AnyRef](implicit clazz: ClassManifest[A]) = new UriPartBuilder {
     def apply(builder: Option[UriBuilder], chain: UriBuilderChain): Option[URI] = {
-      chain(baseUri[A])
+      chain(
+        baseUri[A]
+        .map(UriBuilder.fromUri(_)))
     }
   }
 }
 
-class Links(val context: LinkContext) {
+/**
+ * A link resolver is used to resolve the resource that is used to determine a sub resource.
+ */
+trait LinkResolver[A] {
 
-  def linkTo[A<:AnyRef](implicit clazz: ClassManifest[A]): ResourceActionLinkBuilder[A] = 
-    new ResourceLinkBuilderImpl(context).resolvedBy[A]
+  /**
+   * resolve the link to the given target instance.
+   *
+   * @param target the target instance for which the link should be build
+   * @param builder the link builder to use to fill in the missing parts of the link.
+   *
+   * @return if this link resolver was capable of resolving the link the implementation must return Some linkbuilder instance. Otherwise None.
+   */
+  def apply(target: A, builder: ResourceLinkBuilder): Option[LinkBuilder]
+}
 
+trait UriPartBuilder {
+  def apply(builder: Option[UriBuilder], chain: UriBuilderChain): Option[URI]
+}
+
+/**
+ * the uri builder chain is used internally by the link builders to build the uri.
+ */
+trait UriBuilderChain {
+
+  /**
+   * Continue building the link by using the given uri builder.
+   */
+  def apply(builder: Option[UriBuilder]): Option[URI] = apply(builder, Map[String, AnyRef]())
+
+  /**
+   * Continue building the link by using the given uri builder.
+   * This allows you to define a map of parameters for the final uri.
+   */
+  def apply(builder: Option[UriBuilder], params: Map[String, AnyRef]): Option[URI]
+
+  def apply(builder: Option[UriBuilder], params: (String, AnyRef)*): Option[URI] = apply(builder, Map.empty ++ params)
+
+}
+
+/**
+ * lets you build the uri.
+ */
+trait LinkBuilder {
+  /**
+   * build the actual uri.
+   *
+   * @return None if the link could not be determined or Some(uri) if a valid link could be determined.
+   */
+  def build: Option[URI]
+}
+
+/**
+ * allows you to specify the fragment for the link
+ */
+trait FragmentLinkBuilder extends LinkBuilder {
+  /**
+   * define the fragment of the link.
+   */
+  def fragment(fragment: String): LinkBuilder
+}
+
+/**
+ * Allows to specify by which action a resource is resolved.
+ */
+trait ResourceActionLinkBuilder[A] extends ResourceLinkBuilder {
+  /**
+   * specify the action (method) that is used to resolve the resource. The actual method is not executed.
+   * Only the passed parameters where collected to build the link.
+   * @param call call the method with the parameters that should be used to build the link.
+   * Use null for any @Context annotated or other uri unrelated parameters.
+   * It is also possible to pass null for uri related parameters that have a given default value.
+   */
+  def action(call: (A => Any)): ResourceLinkBuilder
   
-  def linkTo[A<:AnyRef](target: A)(implicit clazz: ClassManifest[A]): ResourceActionLinkBuilder[A] = {
-    new ResourceLinkBuilderImpl(context).resolvedBy(target)
+  /**
+   * define a method which with a @Path annotation to build the link
+   */
+  def action(methodName: String): ResourceLinkBuilder
+
+  def action(methodName: String, params: Map[String, AnyRef]): ResourceLinkBuilder
+
+  def action(methodName: String, params: Seq[AnyRef]): ResourceLinkBuilder
+
+  def action(methodName: String, param: (String, AnyRef), others: (String, AnyRef)*): ResourceLinkBuilder = {
+    action(methodName, Map(param) ++ others)
   }
+
+  def action(methodName: String, param: AnyRef, others: AnyRef*): ResourceLinkBuilder = {
+    action(methodName, Seq(param) ++ others)
+  }
+}
+
+/**
+ * allows you to specify by which other resource the current resource is resolved.
+ */
+trait ResourceLinkBuilder extends FragmentLinkBuilder {
+
+  /**
+   * Specify the root resource class which is capable to resolve the resource.
+   */
+  def resolvedBy[A<:AnyRef](implicit clazz: ClassManifest[A]): ResourceActionLinkBuilder[A]
+
+  /**
+   * specify the resource instance which is capable to resolve the resource.
+   */
+  def resolvedBy[A<:AnyRef](target: A)(implicit clazz: ClassManifest[A]): ResourceActionLinkBuilder[A]
 }
 
 class FragmentLinkBuilderImpl[A](builders: Seq[UriPartBuilder]) extends LinkBuilderImpl(builders)
                                                                 with FragmentLinkBuilder {
-  
+
   def fragment(fragment: String): LinkBuilder = {
     def part = new UriPartBuilder {
       def apply(builder: Option[UriBuilder], chain: UriBuilderChain): Option[URI] = {
@@ -139,7 +242,7 @@ class ResourceLinkBuilderImpl(context: LinkContext,
                               builders: Seq[UriPartBuilder]) extends FragmentLinkBuilderImpl(builders)
                                                              with ResourceLinkBuilder {
   def this(context: LinkContext) = this(context, Seq.empty)
-  
+
   def resolvedBy[A<:AnyRef](implicit clazz: ClassManifest[A]): ResourceActionLinkBuilder[A] = {
     def rootPathBuilder(clazz: Class[_]) = new UriPartBuilder {
       def apply(builder: Option[UriBuilder], chain: UriBuilderChain): Option[URI] = {
@@ -158,7 +261,7 @@ class ResourceLinkBuilderImpl(context: LinkContext,
                                                                    Seq(context.baseUriBuilder(clazz),
                                                                        rootPathBuilder(clazz.erasure)),
                                                                    c)
-      case other => new ResourceActionLinkBuilderImpl[A](context, 
+      case other => new ResourceActionLinkBuilderImpl[A](context,
                                                          Seq(subPathBuilder),
                                                          clazz.erasure.asInstanceOf[Class[A]])
     }
@@ -195,12 +298,8 @@ class ResourceLinkBuilderImpl(context: LinkContext,
   }
 }
 
-trait UriPartBuilder {
-  def apply(builder: Option[UriBuilder], chain: UriBuilderChain): Option[URI]
-}
-
 class LinkBuilderImpl(builders: Seq[UriPartBuilder]) extends LinkBuilder {
-  
+
   def build: Option[URI] = {
 
     def chain(tail: Seq[UriPartBuilder]) = new UriBuilderChain {
@@ -223,122 +322,13 @@ class LinkBuilderImpl(builders: Seq[UriPartBuilder]) extends LinkBuilder {
   }
 }
 
-trait UriBuilderChain {
-  def apply(builder: Option[UriBuilder]): Option[URI] = apply(builder, Map[String, AnyRef]())
+object RootResource {
 
-  def apply(builder: Option[UriBuilder], params: Map[String, AnyRef]): Option[URI]
-
-  def apply(builder: Option[UriBuilder], param: (String, AnyRef), others: (String, AnyRef)*): Option[URI] = others match {
-    case null => apply(builder, Map(param))
-    case more => apply(builder, Map(param) ++ more)
+  def unapply[A<:AnyRef](clazz: ClassManifest[A]): Option[Class[A]] = {
+    unapply(clazz.erasure.asInstanceOf[Class[A]])
   }
 
-}
-
-/**
- * A link resolver is used to resolve the resource that is used to determine a sub resource.
- */
-trait LinkResolver[A] {
-
-  /**
-   * resolve the link to the given target instance.
-   *
-   * @param target the target instance for which the link should be build
-   * @param builder the link builder to use to fill in the missing parts of the link.
-   *
-   * @return if this link resolver was capable of resolving the link the implementation must return Some linkbuilder instance. Otherwise None.
-   */
-  def apply(target: A, builder: ResourceLinkBuilder): Option[LinkBuilder]
-}
-
-trait LinkBuilder {
-  /**
-   * build the actual uri.
-   */
-  def build: Option[URI]
-}
-
-trait FragmentLinkBuilder extends LinkBuilder {
-  /**
-   * define the fragment of the link.
-   */
-  def fragment(fragment: String): LinkBuilder
-}
-
-trait ResourceActionLinkBuilder[A] extends ResourceLinkBuilder {
-  /**
-   * specify the action (method) that is used to resolve the resource. The actual method is not executed.
-   * Only the passed parameters where collected to build the link.
-   * @param call call the method with the parameters that should be used to build the link.
-   * Use null for any @Context annotated or other uri unrelated parameters.
-   * It is also possible to pass null for uri related parameters that have a given default value.
-   */
-  def action(call: (A => Any)): ResourceLinkBuilder
-  
-  /**
-   * define a method which with a @Path annotation to build the link
-   */
-  def action(methodName: String): ResourceLinkBuilder
-
-  def action(methodName: String, params: Map[String, AnyRef]): ResourceLinkBuilder
-
-  def action(methodName: String, params: Seq[AnyRef]): ResourceLinkBuilder
-
-  def action(methodName: String, param: (String, AnyRef), others: (String, AnyRef)*): ResourceLinkBuilder = {
-    action(methodName, Map(param) ++ others)
+  def unapply[A<:AnyRef](clazz: Class[A]): Option[Class[A]] = {
+    Option(clazz.getAnnotation(classOf[Path])).map(x => clazz)
   }
-
-  def action(methodName: String, param: AnyRef, others: AnyRef*): ResourceLinkBuilder = {
-    action(methodName, Seq(param) ++ others)
-  }
-}
-
-trait ResourceLinkBuilder extends FragmentLinkBuilder {
-
-  /**
-   * Specify the root resource class which is capable to resolve the resource.
-   */
-  def resolvedBy[A<:AnyRef](implicit clazz: ClassManifest[A]): ResourceActionLinkBuilder[A]
-
-  /**
-   * specify the resource instance which is capable to resolve the resource.
-   */
-  def resolvedBy[A<:AnyRef](target: A)(implicit clazz: ClassManifest[A]): ResourceActionLinkBuilder[A]
-}
-
-@Path("path")
-class RootResource {
-  @GET
-  def get = None
-
-  @Path("{id}")
-  def get(@PathParam("id") id: String) = None
-}
-
-//@ResolvedBy(classOf[MyResource])
-class SubResource {
-  @GET
-  def getSub = None
-
-  @GET
-  @Path("{id}")
-  def getSub(@PathParam("id") id: String) = None
-}
-
-object Test {
-  val root = new RootResource
-  val sub = new SubResource
-
-  import Links._
-
-  val html = <p>
-    <a href={linkTo(root)}>Link to a root resource with path /path</a>
-    <!-- <a href={linkTo(root).action(_.get("foo"))}>Link to a root resource with path /path/foo</a> -->
-    <a href={linkTo(sub)}>Link to a sub resource. If no link resolver is registered that is capable of resolving a path to such resource a runtime error is thrown.</a>
-    <a href={linkTo(sub).fragment("divid")}>Link to a sub resource with a defined fragment id. If no link resolver is registered that is capable of resolving a path to such resource a runtime error is thrown.</a>
-    <a href={linkTo(sub).resolvedBy[RootResource]}>Link to a sub resource by explicitly defining the root resource.</a>
-    <!-- <a href={linkTo(sub).action(_.getSub("bar"))
-             .resolvedBy[RootResource]
-             .action(_.get("foo"))}>Link to a sub resource resolved by explicitly defining the root resource with path /path/foo/bar</a>-->
-             </p>
 }
--- bundles/web-links/src/test/java/org/mediahub/web/links/LinksTest.scala
+++ bundles/web-links/src/test/java/org/mediahub/web/links/LinksTest.scala
@@ -28,8 +28,8 @@ class LinksTest {
   @Before
   def setUp {
     context = new LinkContext {
-      override def baseUri[A<:AnyRef](implicit clazz: ClassManifest[A]): Option[UriBuilder] = {
-        Some(UriBuilder.fromUri(URI.create("/context")))
+      override def baseUri[A<:AnyRef](implicit clazz: ClassManifest[A]): Option[URI] = {
+        Some(URI.create("/context"))
       }
       override def resolver = Seq(new SomeOtherLinkResolver, new AnyRefLinkResolver, new SomeLinkResolver)
     }
@@ -95,3 +95,39 @@ class MyRootResource {
 }
 
 class MySubResource(val id: String)
+
+@Path("path")
+class RootResource {
+  @GET
+  def get = None
+
+  @Path("{id}")
+  def get(@PathParam("id") id: String) = None
+}
+
+//@ResolvedBy(classOf[MyResource])
+class SubResource {
+  @GET
+  def getSub = None
+
+  @GET
+  @Path("{id}")
+  def getSub(@PathParam("id") id: String) = None
+}
+
+object Foo {
+  val root = new RootResource
+  val sub = new SubResource
+
+  val html =
+  <p>
+    <a href={linkTo(root)}>Link to a root resource with path /path</a>
+    <a href={linkTo(root).action(_.get("foo"))}>Link to a root resource with path /path/foo</a>
+    <a href={linkTo(sub)}>Link to a sub resource. If no link resolver is registered that is capable of resolving a path to such resource a runtime error is thrown.</a>
+    <a href={linkTo(sub).fragment("divid")}>Link to a sub resource with a defined fragment id. If no link resolver is registered that is capable of resolving a path to such resource a runtime error is thrown.</a>
+    <a href={linkTo(sub).resolvedBy[RootResource]}>Link to a sub resource by explicitly defining the root resource.</a>
+    <a href={linkTo(sub).action(_.getSub("bar"))
+                        .resolvedBy[RootResource]
+                        .action(_.get("foo"))}>Link to a sub resource resolved by explicitly defining the root resource with path /path/foo/bar</a>
+  </p>
+}
