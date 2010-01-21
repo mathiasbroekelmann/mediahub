b055a8bd36653a72a2f96db8ed14558715f94390
Mathias Brökelmann <mbroekelmann@googlemail.com>
2010-01-21 17:47:23 +0100

    add some doc to the prototype

--- frontend/html/desktop/src/main/scala/de/osxp/dali/frontend/html/desktop/Views.scala
+++ frontend/html/desktop/src/main/scala/de/osxp/dali/frontend/html/desktop/Views.scala
@@ -3,66 +3,128 @@ package org.mediahub.frontend.html.desktop
 import scala.xml._
 import com.google.inject._
 
-trait Page extends Content {
-  def title: String
-  def teaser: Seq[Content]
-}
-
-trait Content
-
+/**
+ * common view classifier
+ * type A defines the output of the view (String, NodeSeq, ...)
+ */
 abstract case class ViewClassifier[A]
+
+/**
+ * special view classifier which allows the definition of parameters
+ * type A defines the output of the view (String, NodeSeq, ...)
+ * type B defines the view parameters type(s).
+ */
 abstract case class ParamViewClassifier[A, B]
 
-trait ViewModule {
+/**
+ * the view binder allows binding of views
+ */
+trait ViewBinder {
+  /**
+   * bind a common view which doesn't habe view parameters
+   */
   def bindView[A](classifier: ViewClassifier[A]): ViewBindingBuilder[A]
+
+  /**
+   * bind a view with parameters.
+   */
   def bindView[A, B](classifier: ParamViewClassifier[A, B]): ParamViewBindingBuilder[A, B]
-
-  def render[A<:Any](bean: A): IncludeViewBuilder[A]
 }
 
-trait ParamViewBindingBuilder[A, B] {
-  def to[C<:Any](f: (C => (B => A))): Unit
-}
-
+/**
+ * bind the actual view.
+ */
 trait ViewBindingBuilder[A] {
   def to[B<:Any](f: (B => A)): Unit
 
-  // add more to ... methods for other kinds of view implemntations.
+  // add more to ... methods for other kinds of view implementations.
 }
 
-trait IncludeViewBuilder[A] {
+/**
+ * bind the actual parameterized view.
+ */
+trait ParamViewBindingBuilder[A, B] {
+  def to[C<:Any](f: (C => (B => A))): Unit
+
+  // add more to ... methods for other kinds of view implementations.
+}
+
+/**
+ * the contract for a view module that defines the #render function to include other views.
+ */
+trait ViewModule extends ViewBinder {
 
   /**
-   * include the view for the given classifier
+   * start defining an inclusion of a view for a given instance.
+   */
+  def include[A<:Any](bean: A): IncludeViewBuilder
+
+  /**
+   * start defining to render a view for a given instance.
+   * convinience function to use where include doesn't sound good.
+   */
+  def render[A<:Any](bean: A): IncludeViewBuilder = include(bean)
+}
+
+/**
+ * Allows the definition of a view classifier for a included view
+ *
+ * @TODO: find a better name than IncludeViewBuilder
+ */
+trait IncludeViewBuilder {
+
+  /**
+   * render the given common view for the previously defined bean instance.
    */
   def as[B](classifier: ViewClassifier[B]): B
 
+  /**
+   * render the given parameterized view for the previously defined bean instance.
+   */
   def as[B, C](classifier: ParamViewClassifier[B, C]): ParamIncludeViewBuilder[B, C]
 }
 
 /**
- * A: bean type
+ * provide the parameter(s) for the defined view.
+ *
+ * A: view output type
  * B: param type
  */
 trait ParamIncludeViewBuilder[A, B] {
   def withParameter(p: B): A
 }
 
+// example code to verify stuff is working
+// domain class
+trait SomePage extends Content {
+  def title: String
+  def teaser: Seq[Content]
+}
+
+// domain class
+trait Content
+
+// example view classifiers defining the result of a view
 object Body extends ViewClassifier[NodeSeq]
 object Content extends ViewClassifier[NodeSeq]
+// example view classifier which accepts parameters. This defines the types of the parameters
 object ParamView extends ParamViewClassifier[NodeSeq, (String, Content)]
 
+// example module which binds some views
 trait  MyViewModule extends ViewModule {
-  bindView(Body).to { page: Page =>
+
+  // bind a view of Page to the classifier body
+  bindView(Body).to { page: SomePage =>
     <body>
       <h1>{page.title}</h1>
       {
         <!-- variable assignment -->
         val pageTeaser = page.teaser
-        <!-- iteration over content -->
+        <!-- verify condition -->
         if(!pageTeaser.isEmpty) {
           <ul>
             {
+              <!-- iteration over content -->
               for(teaser <- pageTeaser) yield {
                 <li>
                   <!-- render a nested view -->
@@ -74,21 +136,40 @@ trait  MyViewModule extends ViewModule {
         }
       }
       <div>
+        <!-- render a parameterized view -->
         {render(page) as ParamView withParameter ("foo", page.teaser.head)}
+        <!-- this will not pass the compiler: -->
+        <!-- {render(page) as ParamView withParameter ("foo", 1234)} -->
       </div>
     </body>
   }
 
-  bindView(Content) to { page: Page =>
+  /**
+   * just a plain view binding
+   */
+  bindView(Content) to { page: SomePage =>
     <div>
       <div>{page.title}</div>
     </div>
   }
 
-  bindView(ParamView) to { page: Page =>
+  /**
+   * example for a view binding which provides additional parameters
+   */
+  bindView(ParamView) to { page: SomePage =>
+    // param is of type (String, Content)
     { param =>
+      // str is implicit type String
+      // content is implicit type Content
       val (str, content) = param
       NodeSeq.Empty
     }
   }
+
+  /**
+   * This would not compile!
+  bindView(ParamView) to { page: SomePage =>
+      NodeSeq.Empty
+  }
+   */
 }
