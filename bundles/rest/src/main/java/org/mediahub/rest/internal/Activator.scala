3134137babee3aff4f0a7ac5ab6ce4609726c8a4
Mathias Brökelmann <mbroekelmann@googlemail.com>
2010-01-28 22:56:23 +0100

    implement activator for rest module

--- bundles/rest/src/main/java/org/mediahub/rest/internal/Activator.scala
+++ bundles/rest/src/main/java/org/mediahub/rest/internal/Activator.scala
@@ -8,12 +8,53 @@ package org.mediahub.rest.internal
 import org.osgi.framework._
 import org.mediahub.rest._
 
-class Activator extends BundleActivator {
-  def start(ctx: BundleContext) {
+import org.restmodules.filter.FilterRegistrar
 
+import org.osgi.util.tracker._
+
+import scala.actors.Actor
+
+import javax.ws.rs.core.Application
+
+class Activator extends BundleActivator {
+
+  def registry(ctx: BundleContext) = new ApplicationRegistry {
+    def register(app: Application) = {
+      new Registration {
+        val registration = ctx.registerService(classOf[Application].getName, app, null)
+        def unregister = registration.unregister
+      }
+    }
+  }
+  
+  def tracker(ctx: BundleContext, collector: Actor) = new ServiceTrackerCustomizer {
+    def addingService(reference: ServiceReference): AnyRef = {
+      val service = ctx.getService(reference)
+      collector ! Added(service)
+      service
+    }
+    def modifiedService(reference: ServiceReference, service: AnyRef) {
+    }
+
+    def removedService(reference: ServiceReference, service: AnyRef) {
+      collector ! Removed(service)
+    }
+  }
+
+  private[this] var trackedServices = List.empty[ServiceTracker]
+
+  def track[A](ctx: BundleContext, listener: ServiceTrackerCustomizer)(implicit clazz: ClassManifest[A]) = {
+    new ServiceTracker(ctx, clazz.erasure.getName, listener)
+  }
+
+  def start(ctx: BundleContext) {
+    val registration = new RestApplicationRegistration(registry(ctx))
+    implicit val serviceTracker = tracker(ctx, registration.collector)
+    trackedServices = track[RestRegistrar](ctx, serviceTracker) :: track[FilterRegistrar](ctx, serviceTracker) :: Nil
+    trackedServices.foreach(_.open)
   }
 
   def stop(ctx: BundleContext) {
-    
+    trackedServices.foreach(_.close)
   }
 }
